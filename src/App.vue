6bf913f73f512186702ca9e3ad013d163ddb80b2
Piyumra Jayarathne <piyumrajayarathne@gmail.com>
2026-03-01 17:27:37 +0530

    Initial Vue 3 + Tailwind setup

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="h-screen bg-green-500 flex items-center justify-center">
+    <h1 class="text-6xl text-white font-bold">
+      TAILWIND TEST
+    </h1>
+  </div>
+</template>
